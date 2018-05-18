0e8014bef6b63174d820f743c39381c07605a16b
Franz Kohnle <franz@kohnlehome.de>
2018-05-18 08:14:47 +0200

    Fehler eingebaut

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
+  FEHLER eingebaut
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
 }).listen(1337, '127.0.0.1');
